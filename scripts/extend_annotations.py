c7b631b7dd871cb8d61bc965685a248872749a25
Dominik Lindner <d.lindner@dundee.ac.uk>
2022-03-31 13:55:51 +0100

    Split gene columns

--- scripts/extend_annotations.py
+++ scripts/extend_annotations.py
@@ -1,4 +1,4 @@
-import sys
+import io
 import csv
 import re
 import argparse
@@ -10,7 +10,7 @@ called EH-15740-001.svs might be imported as three images
 EH-15740-001.svs [0], EH-15740-001.svs [label image] and EH-15740-001.svs [macro image],
 but the annotations.csv only has one entry for the EH-15740-001.svs file.
 Then you can run:
-extend_annotations.py annotations.csv -r ' [0]' ' [label image]' ' [macro image]'
+python extend_annotations.py annotations.csv -r ' [0]' ' [label image]' ' [macro image]' > out.csv
 to fix the annotations.
 """
 
@@ -21,19 +21,18 @@ parser.add_argument("file", help="Annotations file")
 parser.add_argument("extensions", help="The extension to add", nargs='+')
 args = parser.parse_args()
 
-new_file = re.sub('.csv$', '_new.csv', args.file)
-
 with open(args.file, mode='r') as csv_file_in:
     csv_reader = csv.DictReader(csv_file_in)
     org_rows = list(csv_reader)
 
-    with open(new_file, mode='w') as csv_file_out:
-        csv_writer = csv.DictWriter(csv_file_out, fieldnames=csv_reader.fieldnames)
-        csv_writer.writeheader()
-        for org_row in org_rows:
-            if not args.r:
-                csv_writer.writerow(org_row)
-            org_image_name = org_row["Image Name"]
-            for extension in args.extensions:
-                org_row["Image Name"] = f"{org_image_name}{extension}"
-                csv_writer.writerow(org_row)
+    output = io.StringIO()
+    csv_writer = csv.DictWriter(output, fieldnames=csv_reader.fieldnames)
+    csv_writer.writeheader()
+    for org_row in org_rows:
+        if not args.r:
+            csv_writer.writerow(org_row)
+        org_image_name = org_row["Image Name"]
+        for extension in args.extensions:
+            org_row["Image Name"] = f"{org_image_name}{extension}"
+            csv_writer.writerow(org_row)
+    print(output.getvalue())
